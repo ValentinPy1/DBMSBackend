63da6d1009db0bd7114920c1b9c67482c6b3c2e5
bugrahanyaman <byaman20@ku.edu.tr>
2025-01-22 17:41:10 +0300

    Fix column names in endpoints

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -22,7 +22,7 @@ def get_db_connection():
         connection = mysql.connector.connect(
             host=os.getenv('DB_HOST'),
             user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
+            # password=os.getenv('DB_PASSWORD'),
             database=os.getenv('DB_NAME')
         )
         print("Database connection successful!")
